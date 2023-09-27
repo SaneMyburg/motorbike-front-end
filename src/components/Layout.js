7d5eb26662194874b5966ca2a8019509325e58c7
SaneMyburg <sane.myburg@gmail.com>
2023-09-27 18:57:31 +0200

    Fix linters

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,22 +5,10 @@ import Home from './Home';
 const Layout = () => (
   <div className="container-fluid">
     <div className="row flex-nowrap">
-
       <Navbar />
-       <Home />
-      <div className="col py-3">
-        <p className="lead">
-      
-        </p>
-        <p>
-  
-        </p>
-        <ul className="list-unstyled">
-        </ul>
-      </div>
+      <Home />
+      <div className="col py-3" />
     </div>
-
   </div>
 );
-
 export default Layout;
