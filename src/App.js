cd95e968aca7c0b530da6d498ca09c78e0d031cf
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-27 09:48:47 +0430

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <h1>My Motorbikes</h1>
+      <Layout />
     </div>
   );
 }
