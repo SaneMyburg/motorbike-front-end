6d7456655b31e0bba47e90382804f30559dd2e23
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-27 10:36:24 +0430

    Update Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,32 @@
-import React from "react";
-import Navbar from "./navigation";
+import React from 'react';
+import Navbar from './navigation';
 
-const Layout=() =>{
-    return (
-        <div class="container-fluid">
-        <div class="row flex-nowrap">
-    
-            < Navbar/>
-            <div class="col py-3">
-                <h3>Left Sidebar with Submenus</h3>
-                <p class="lead">
-                    An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
-                    menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.</p>
-                <ul class="list-unstyled">
-                    <li><h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile</li>
-                </ul>
-            </div>
-        </div>
+const Layout = () => (
+  <div className="container-fluid">
+    <div className="row flex-nowrap">
 
-        </div>
-    )}
+      <Navbar />
+      <div className="col py-3">
+        <h3>Left Sidebar with Submenus</h3>
+        <p className="lead">
+          An example 2-level sidebar with collasible menu items.
+          The menu functions like an accordion where only a single
+        </p>
+        <p>
+          menu is be open at a time.While the sidebar itself is not
+          toggle-able, it does responsively shrink in width on smaller screens.
+        </p>
+        <ul className="list-unstyled">
+          <li>
+            <h5>Responsive</h5>
+            {' '}
+            shrinks in width, hides text labels and collapses to icons only on mobile
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+);
 
 export default Layout;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,42 +1,38 @@
-import '../style/navigation.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const links = [
+  { path: '/', text: 'Motorbick' },
+  { path: 'newpath', text: 'Reserve' },
+  { path: 'newpath2', text: 'My reservations' },
+  { path: 'newpath3', text: 'ADD Motorbick' },
+  { path: 'newpath3', text: 'Delete Motorbick' },
+];
 
 const NavigationPanel = () => (
-  <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 border-end">
-  <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-      <a href="/" class="d-flex align-items-center pt-5 pb-5 mb-md-0 me-md-auto text-white text-decoration-none">
-          <span class="fs-5 d-none d-sm-inline text-black">Motorbick</span>
-      </a>
-      <ul class="nav nav-pills flex-column mb-sm-auto mt-3 pt-3 mb-0 align-items-center align-items-sm-start" id="menu">
-          <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0 .active">
-                   <span class="ms-1 d-none d-sm-inline text-black"> New Appointment</span>
-              </a>
+  <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 border-end">
+    <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+      <button type="button" className="d-flex align-items-center pt-5 pb-5 mb-md-0 me-md-auto text-white text-decoration-none">
+        <span className="fs-5 d-none d-sm-inline text-black">Logo</span>
+      </button>
+      <ul className="nav nav-pills flex-column mb-sm-auto mt-3 pt-3 mb-0 align-items-center align-items-sm-start" id="menu">
+        {links.map((link) => (
+          <li className="nav-item" key={link.text}>
+            <NavLink to={link.path}>
+              {link.text}
+            </NavLink>
           </li>
-          <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                   <span class="ms-1 d-none d-sm-inline"> MY Appointment</span>
-              </a>
-          </li>
-          <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                   <span class="ms-1 d-none d-sm-inline"> ADD Motorbick</span>
-              </a>
-          </li>
-          
-          <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                   <span class="ms-1 d-none d-sm-inline"> Delete Motorbick</span>
-              </a>
-          </li>
+        ))}
       </ul>
-      <hr/>
-      <div class="dropdown pb-4">
-          <ul>
-              <li><a class="" href="#">Sign out</a></li>
-          </ul>
+      <hr />
+      <div className="dropdown pb-4">
+        <ul>
+          <li><a className="" href="user">Sign out</a></li>
+        </ul>
       </div>
+    </div>
   </div>
-</div>
 );
 
 export default NavigationPanel;
