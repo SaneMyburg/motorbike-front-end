f1ed627689aa61fd347b76d075c28a87e594487c
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-27 09:51:35 +0430

    Add layout

---
+++ src/components/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./navigation";
+
+const Layout=() =>{
+    return (
+        <div class="container-fluid">
+        <div class="row flex-nowrap">
+    
+            < Navbar/>
+            <div class="col py-3">
+                <h3>Left Sidebar with Submenus</h3>
+                <p class="lead">
+                    An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single 
+                    menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width on smaller screens.</p>
+                <ul class="list-unstyled">
+                    <li><h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile</li>
+                </ul>
+            </div>
+        </div>
+
+        </div>
+    )}
+
+export default Layout;
