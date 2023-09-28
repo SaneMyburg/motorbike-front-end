4a1083b44cd4bffab6b6c5b3749c5474025a80a5
SaneMyburg <sane.myburg@gmail.com>
2023-09-28 14:38:18 +0200

    Add navigation into home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import store from './redux/store';
 import Home from './components/Home';
 import './App.css';
 import Layout from './components/Layout';
+import Reserve from './components/Reserve';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Layout>
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/reserve/:motorId" element={<Reserve />} />
+          </Routes>
         </Layout>
       </div>
     </Provider>
