79f3607c777af3c2901f9fee842171e2c736cc68
SaneMyburg <sane.myburg@gmail.com>
2023-09-29 21:56:58 +0200

    Create reservations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
 import { fetchMotorbikes } from '../redux/motorbikes/motorbikes';
+import Motor from './Motor';
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMotorbikes());
   }, [dispatch]);
-  
   const motors = useSelector((state) => state.motorbikes.motors);
+
   return (
     <div className="text-center w-75">
       <h2 className="text-center m-4">LATEST MODELS</h2>
       <p className="text-center header-text2 m-2">Please select a model</p>
       <div className="show-motor">
-        {motorbikesData.length > 0 ? (
+        {motors.length > 0 ? (
           <div className="row">
-            {console.log('Length of motorbikesData:', motorbikesData.length)}
-            {motorbikesData.map((motorbike) => (
-              <div className="col-md-4" key={motorbike.id}>
-                <div className="motorbike-card">
-                  <h2>{motorbike.name}</h2>
-                  <img src={motorbike.photo} alt={motorbike.name} className="motor-image" />
-                  <p>{motorbike.description}</p>
-                  <p>
-                    Price: $
-                    {motorbike.price}
-                  </p>
-                </div>
+            {motors.map((motor) => (
+              <div className="col-md-4" key={motor.id}>
+                <Motor motor={motor} />
               </div>
             ))}
           </div>
