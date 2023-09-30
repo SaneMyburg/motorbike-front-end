14f7b19c61fdda7c5f41e232f21ccc154ca95e15
SaneMyburg <sane.myburg@gmail.com>
2023-09-30 10:24:48 +0200

    Add reservations reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import motorbikesReducer from './motorbikes/motorbikesReducer';
 import userReducer from './users/userSlice';
+import reservationReducer from './reservations/reservationSlice';
 
 const rootReducer = combineReducers({
   motorbikes: motorbikesReducer,
   users: userReducer,
-  // Add more reducers if you have them
+  reservations: reservationReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
